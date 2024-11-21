001b4d52bfa045c24b36134609cc35e3622e759d
Karan0457 <jagnekaran26@gmail.com>
2024-11-21 21:46:37 +0530

    karanjagne: Call const with addvalue function

--- main2.py
+++ main2.py
@@ -3,9 +3,24 @@ class firstClass:
     val1 = 0
     val2 = 0
 
+    def __init__(self, a, b):
+        print(' this is constructor')
+        self.val1 = a
+        self.val1 = b
 
-f1 = firstClass()
+
+    def addValue(self):
+        print(f'addition = {self.val1 + self.val2}')
+
+
+
+f1 = firstClass(6,7)
+
+f1.addValue()
 
 print('f1.val1 =', f1.val1)
-print('f1.val2 =', f1.val2)
+print(f'f1.val2 = {f1.val2}')
+
+f2 = firstClass(30,40)
+f2.addValue()
 
